3e538cfe97132df9ce4fd7c51407755d185f8bae
Edgaras Čiburys <edgarasciburys@gmail.com>
2017-01-02 22:40:33 +0200

    CustomerInfo web config fix and refactoring

--- Data/CustomerAccounts.cs
+++ Data/CustomerAccounts.cs
@@ -12,7 +12,7 @@ namespace Data
         public int Id { get; set; }
         public string AccountNumber { get; set; }
         public DateTime DateCreated { get; set; }
-        public int CustomerId { get; set}
+        public int CustomerId { get; set; }
         public decimal Balance { get; set; }
         public string AccountType { get; set; }
     }
