d489eed7e868fc019e9421da18d87d54369c096a
Chibur <edgarasciburys@gmail.com>
2017-01-04 17:11:19 +0200

    Template queries in data access layer

--- Data/DataAccessLayer.cs
+++ Data/DataAccessLayer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Odbc;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,61 @@ namespace Data
 {
     class DataAccessLayer
     {
+        private OdbcConnection _connection;
+        private OdbcDataAdapter _adapter;
+
+        public DataAccessLayer()
+        {
+            _connection = new OdbcConnection();
+            _adapter = new OdbcDataAdapter();
+            _connection.ConnectionString = "Driver={SQL Server}; Server={}; Database=CustomerPayments.Database; Integrated Security = False"; 
+        }
+
+        public CustomerInfo GetCustomer(int Id)
+        {
+            try
+            {
+                _connection.Open();
+                OdbcCommand command = _connection.CreateCommand();
+                command.CommandText = "SQL querry to get from db";
+                DataSet data = new DataSet();
+                _adapter.SelectCommand = command;
+                _adapter.Fill(data, "Table name");
+
+                DataTable dt = data.Tables[0];
+                // map all the fields
+                //string id = dt.Rows[0]["ID"].ToString();
+                return new CustomerInfo();
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            finally
+            {
+                _connection.Close();
+            }
+            
+        }
+
+        public bool CreateCustomer(CustomerInfo customer)
+        {
+            try
+            {
+                _connection.Open();
+                OdbcCommand command = _connection.CreateCommand();
+                command.CommandText = "Querry string to modify db";
+                command.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            finally
+            {
+                _connection.Close();
+            }  
+        }
     }
 }
