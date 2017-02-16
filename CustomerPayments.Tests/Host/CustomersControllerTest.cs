882d4e421e307fd9d04d407af2511d8deb38bc19
Edgaras Čiburys <edgarasciburys@gmail.com>
2017-02-16 19:55:44 +0200

    Setting up Moq, AutoFixture, WebApi for testing. GetReturnsCustomer test

--- CustomerPayments.Tests/Host/CustomersControllerTest.cs
+++ CustomerPayments.Tests/Host/CustomersControllerTest.cs
@@ -10,27 +10,38 @@ using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
 using CustomerPayments.DTO;
+using Ploeh.AutoFixture;
+using System.Web.Http;
+using System.Web.Http.Results;
 
 namespace CustomerPayments.Tests.Host
 {
     [TestFixture]
     public class CustomersControllerTest
     {
+        // TODO maybe it is possible to put fixture and  Mock one place and not to reinit it every time
         [Test]
-        public void GetReturnsCustomers()
+        public void GetReturnsCustomer()
         {
             //Arrange
-            var repository = new Mock<IGenericRepository<Customer>>().Object;
+            Fixture fixture = new Fixture();
+            var id = fixture.Create<int>();
+            Customer customer = fixture.Create<Customer>();
+
+            var repository = new Mock<IGenericRepository<Customer>>();
+            repository.Setup(c => c.FindById(id)).Returns(customer);
+
             var mapper = new Mock<IMapper>();
             mapper.Setup(m => m.Map<Customer, CustomerDTO>(It.IsAny<Customer>())).Returns(new CustomerDTO());
-            var controller = new CustomersController(repository, mapper.Object);
-            controller.Request = new HttpRequestMessage();
-            controller.Configuration = new HttpConfiguration();
+            var customerDTO = mapper.Object.Map<CustomerDTO>(customer);
+
+            var controller = new CustomersController(repository.Object, mapper.Object);
 
             //Act
-            var response = controller.Get(1);
+            IHttpActionResult response = controller.Get(id);
+            var responseResult = response as OkNegotiatedContentResult<CustomerDTO>;
             //Assert
-
+            Assert.That(responseResult.Content, Is.EqualTo(customerDTO));
         }
     }
 }
